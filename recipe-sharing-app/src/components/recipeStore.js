f99dbbef7600901fd7c41a92eca179ba1eb08499
munyu_vsl <munyu.vsl@gmail.com>
2025-07-28 11:33:58 +0300

    Add recipe details, edit, and delete functionality with Zustand and routing

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,18 +1,31 @@
 import { create } from 'zustand';
 
-export const useRecipeStore = create((set) => ({
+export const useRecipeStore = create((set, get) => ({
   recipes: [],
+  searchTerm: '',
+  setSearchTerm: (term) => set({ searchTerm: term }),
+
   addRecipe: (newRecipe) =>
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+
   updateRecipe: (updatedRecipe) =>
     set((state) => ({
       recipes: state.recipes.map((recipe) =>
         recipe.id === updatedRecipe.id ? updatedRecipe : recipe
       ),
     })),
+
   deleteRecipe: (id) =>
     set((state) => ({
       recipes: state.recipes.filter((recipe) => recipe.id !== id),
     })),
+
   setRecipes: (recipes) => set({ recipes }),
+
+  filteredRecipes: () => {
+    const { recipes, searchTerm } = get();
+    return recipes.filter((r) =>
+      r.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  },
 }));
