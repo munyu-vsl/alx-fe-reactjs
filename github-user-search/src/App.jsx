e47896bf29f3812120df536b2fb75e3524188121
munyu_vsl <munyu.vsl@gmail.com>
2025-08-02 20:31:27 +0300

    Initial setup for GitHub User Search React app

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>GitHub User Search</h1>
+      {/* Future components will go here */}
+    </div>
+  );
+}
+
+export default App;
