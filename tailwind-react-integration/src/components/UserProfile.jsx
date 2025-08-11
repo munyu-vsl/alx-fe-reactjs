481760f7d074bc3bcb55140c7ba9689137490116
munyu_vsl <munyu.vsl@gmail.com>
2025-08-11 18:10:44 +0300

    Add responsive components to the user profile

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -16,7 +16,7 @@ function UserProfile() {
         alt="User"
         className="
           rounded-full
-          w-24 h-24 md:w-36 md:h-36
+          sm:w-24 sm:h-24 md:w-36 md:h-36
           mx-auto
         "
       />
